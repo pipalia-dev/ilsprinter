f4c446de376cd04f24e3768d465a4521db3aefe0
Pipalia Dev <dev@pipalia.co.uk>
2021-02-13 18:09:57 +0100

    - Printer #3

--- src/main/java/co/uk/ils/printing/PrintingApi.java
+++ src/main/java/co/uk/ils/printing/PrintingApi.java
@@ -7,10 +7,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.print.PrintException;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
+import javax.print.*;
+import javax.print.attribute.AttributeSet;
+import javax.print.attribute.HashPrintServiceAttributeSet;
+import javax.print.attribute.standard.PrinterName;
+import java.awt.*;
+import java.awt.print.*;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -28,17 +32,79 @@ public class PrintingApi {
                 + "Return Address: \n"
                 + "I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL";
 
-        Barcode barcode = BarcodeFactory.createCode128(label);
-        PrinterJob job = PrinterJob.getPrinterJob();
-        job.setPrintable(barcode);
-        job.print();
-//        PrinterService printerService = new PrinterService();
-//        printerService.printString(label);
+        feedPrinter(label.getBytes(StandardCharsets.UTF_8));
         return ResponseEntity.ok("ok");
     }
 
+    @RequestMapping(path = "/print2", method = GET)
+    public ResponseEntity<String> print2() throws PrintException, PrinterException, BarcodeException {
+        String label = "JOSIE CAMPBELL\n"
+                + "FOXHOLLOW\n"
+                + "STOKE HILL, CHEW STOKE\n"
+                + "BRISTOL\n"
+                + "AVON\n"
+                + "BS40 8XG\n"
+                + "\n"
+                + "Return Address: \n"
+                + "I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL";
+
+        final PrinterJob job = PrinterJob.getPrinterJob();
+
+        Printable contentToPrint = new Printable() {
+            @Override
+            public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+                Graphics2D g2d = (Graphics2D) graphics;
+                g2d.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+                g2d.setFont(new Font("Monospaced", Font.BOLD, 7));
+
+                if (pageIndex > 0) {
+                    return NO_SUCH_PAGE;
+                } //Only one page
+
+                g2d.drawString(label, 0, 0);
+
+                return PAGE_EXISTS;
+            }
+        };
+
+        PageFormat pageFormat = new PageFormat();
+        pageFormat.setOrientation(PageFormat.PORTRAIT);
+
+        Paper pPaper = pageFormat.getPaper();
+        pPaper.setImageableArea(0, 0, pPaper.getWidth() , pPaper.getHeight() -2);
+        pageFormat.setPaper(pPaper);
+
+        job.setPrintable(contentToPrint, pageFormat);
+
+        try {
+            job.print();
+        } catch (PrinterException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return ResponseEntity.ok("ok");
+    }
     @RequestMapping(path = "/health", method = GET)
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("ok");
+    }
+
+    private static boolean feedPrinter(byte[] b) {
+        try {
+            DocPrintJob job = PrintServiceLookup.lookupDefaultPrintService().createPrintJob();
+            DocFlavor flavor = DocFlavor.BYTE_ARRAY.AUTOSENSE;
+            Doc doc = new SimpleDoc(b, flavor, null);
+
+            job.print(doc, null);
+            Thread.sleep(1000);
+            System.out.println("Done !");
+        } catch (javax.print.PrintException pex) {
+            System.out.println("Printer Error " + pex.getMessage());
+            return false;
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
