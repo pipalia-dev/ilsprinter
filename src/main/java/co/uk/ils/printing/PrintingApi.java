59325aec97d59f31f1de1a81f7cc6cc258fdcee7
Pipalia Dev <dev@pipalia.co.uk>
2021-02-13 17:50:12 +0100

    - Printer #2

--- src/main/java/co/uk/ils/printing/PrintingApi.java
+++ src/main/java/co/uk/ils/printing/PrintingApi.java
@@ -1,10 +1,15 @@
 package co.uk.ils.printing;
 
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeException;
+import net.sourceforge.barbecue.BarcodeFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.print.PrintException;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
 import java.io.UnsupportedEncodingException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
@@ -12,7 +17,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Controller
 public class PrintingApi {
     @RequestMapping(path = "/print", method = GET)
-    public ResponseEntity<String> print() throws PrintException {
+    public ResponseEntity<String> print() throws PrintException, PrinterException, BarcodeException {
         String label = "JOSIE CAMPBELL\n"
                 + "FOXHOLLOW\n"
                 + "STOKE HILL, CHEW STOKE\n"
@@ -23,8 +28,12 @@ public class PrintingApi {
                 + "Return Address: \n"
                 + "I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL";
 
-        PrinterService printerService = new PrinterService();
-        printerService.printString(label);
+        Barcode barcode = BarcodeFactory.createCode128(label);
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(barcode);
+        job.print();
+//        PrinterService printerService = new PrinterService();
+//        printerService.printString(label);
         return ResponseEntity.ok("ok");
     }
 
