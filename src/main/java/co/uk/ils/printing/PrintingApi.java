88c99fa7e158116320d39c63f7cc6d293bc66991
Pipalia Dev <dev@pipalia.co.uk>
2021-02-14 18:01:31 +0100

    - Printer #16

--- src/main/java/co/uk/ils/printing/PrintingApi.java
+++ src/main/java/co/uk/ils/printing/PrintingApi.java
@@ -3,6 +3,7 @@ package co.uk.ils.printing;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import javax.print.PrintServiceLookup;
 import javax.print.attribute.HashPrintRequestAttributeSet;
@@ -11,18 +12,20 @@ import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.MediaPrintableArea;
 import javax.print.attribute.standard.OrientationRequested;
 import javax.swing.JTextPane;
-import javax.swing.text.*;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.Insets;
-import java.awt.print.PrinterException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class PrintingApi {
-    @RequestMapping(path = "/print", method = GET)
-    public ResponseEntity<String> print()  {
+    @RequestMapping(path = "/test-print", method = GET)
+    public ResponseEntity<String> testPrint()  {
         List<String> bigFontText = new ArrayList<>();
         bigFontText.add("JOSIE CAMPBELL\n");
         bigFontText.add("FOXHOLLOW\n");
@@ -32,27 +35,44 @@ public class PrintingApi {
         bigFontText.add("BS40 8XG\n");
         bigFontText.add("\n");
 
-        List<String> smallText = new ArrayList<>();
-        smallText.add("Return Address: \n");
-        smallText.add("I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL");
+        List<String> smallFontText = new ArrayList<>();
+        smallFontText.add("Return Address: \n");
+        smallFontText.add("I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL");
 
         try {
-            JTextPane textPane = new JTextPane();
-
-            Document document = textPane.getStyledDocument();
-            document.insertString(document.getLength(), toString(bigFontText), getFont(16));
-            document.insertString(document.getLength(), toString(smallText), getFont(10));
-
-            PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-            attributes.add(OrientationRequested.LANDSCAPE);
-            attributes.add(new MediaPrintableArea(0,0,50, 97, Size2DSyntax.MM));
-            textPane.setMargin(new Insets(20, 20, 20, 20));
-
-            textPane.print(null, null, true, PrintServiceLookup.lookupDefaultPrintService(), attributes, true);
-            return ResponseEntity.ok("Printed");
-        } catch (PrinterException | BadLocationException e) {
+            print(bigFontText, smallFontText);
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+        return ResponseEntity.ok("Printed");
+    }
+
+    @RequestMapping(path = "/print", method = POST)
+    public ResponseEntity<String> print(@RequestBody List<PrintingData> dtos) {
+        try {
+            for (PrintingData dto : dtos) {
+                print(dto.bigFontLines, dto.smallFontLines);
+            }
+
+            return ResponseEntity.ok("Printed");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+
+    public void print(List<String> bigFontText, List<String> smallFontText) throws Exception {
+        JTextPane textPane = new JTextPane();
+
+        Document document = textPane.getStyledDocument();
+        document.insertString(document.getLength(), toString(bigFontText), getFont(16));
+        document.insertString(document.getLength(), toString(smallFontText), getFont(10));
+
+        PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+        attributes.add(OrientationRequested.LANDSCAPE);
+        attributes.add(new MediaPrintableArea(0, 0, 50, 97, Size2DSyntax.MM));
+        textPane.setMargin(new Insets(20, 20, 20, 20));
+
+        textPane.print(null, null, false, PrintServiceLookup.lookupDefaultPrintService(), attributes, false);
     }
 
     private String toString(List<String> lines) {
---
+++ src/main/java/co/uk/ils/printing/PrintingData.java
@@ -0,0 +1,8 @@
+package co.uk.ils.printing;
+
+import java.util.List;
+
+public class PrintingData {
+    public List<String> bigFontLines;
+    public List<String> smallFontLines;
+}
