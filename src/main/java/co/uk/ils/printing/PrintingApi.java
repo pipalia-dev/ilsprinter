99df73816c7ded5e96faf82b71bfd0f382e6ce5f
Pipalia Dev <dev@pipalia.co.uk>
2021-02-14 15:41:58 +0100

    - Printer #13

--- src/main/java/co/uk/ils/printing/PrintingApi.java
+++ src/main/java/co/uk/ils/printing/PrintingApi.java
@@ -1,67 +1,45 @@
 package co.uk.ils.printing;
 
-import net.sourceforge.barbecue.Barcode;
-import net.sourceforge.barbecue.BarcodeException;
-import net.sourceforge.barbecue.BarcodeFactory;
-import net.sourceforge.barbecue.BarcodeImageHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import javax.print.PrintServiceLookup;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
 import javax.print.attribute.Size2DSyntax;
 import javax.print.attribute.standard.MediaPrintableArea;
-import javax.print.attribute.standard.MediaSizeName;
 import javax.print.attribute.standard.OrientationRequested;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.print.PageFormat;
-import java.awt.print.Paper;
-import java.awt.print.Printable;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import java.awt.Insets;
 import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
-import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 public class PrintingApi {
     @RequestMapping(path = "/print", method = GET)
-    public ResponseEntity<String> print() throws PrinterException, BarcodeException {
-        String label = "JOSIE CAMPBELL\n"
-                + "FOXHOLLOW\n"
-                + "STOKE HILL, CHEW STOKE\n"
-                + "BRISTOL\n"
-                + "AVON\n"
-                + "BS40 8XG\n"
-                + "\n"
-                + "Return Address: \n"
-                + "I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL";
+    public ResponseEntity<String> print()  {
+        List<String> bigFontText = new ArrayList<>();
+        bigFontText.add("JOSIE CAMPBELL\n");
+        bigFontText.add("FOXHOLLOW\n");
+        bigFontText.add("STOKE HILL, CHEW STOKE\n");
+        bigFontText.add("BRISTOL\n");
+        bigFontText.add("AVON\n");
+        bigFontText.add("BS40 8XG\n");
+        bigFontText.add("\n");
 
-        Barcode barcode = BarcodeFactory.create2of7(label);
-        barcode.setBarHeight(60);
-        barcode.setBarWidth(2);
+        List<String> smallText = new ArrayList<>();
+        smallText.add("Return Address: \n");
+        smallText.add("I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL");
 
-        PrinterJob job = PrinterJob.getPrinterJob();
-        job.setPrintable(barcode);
-        job.print();
-        return ResponseEntity.ok("ok");
-    }
-
-    public static void main(String[] args) throws PrinterException {
-        String label = "JOSIE CAMPBELL\n"
-                + "FOXHOLLOW\n"
-                + "STOKE HILL, CHEW STOKE\n"
-                + "BRISTOL\n"
-                + "AVON\n"
-                + "BS40 8XG\n"
-                + "\n"
-                + "Return Address: \n"
-                + "I.L.S Schools, Unit 2 Sovereign Park, Laporte Way, Luton, Beds, LU4 8EL";
         JTextPane textPane = new JTextPane();
+
+        textPane.setFont(new Font(Font.SANS_SERIF, 3, 9));
+        textPane.setText(toString(bigFontText));
 
         textPane.setFont(new Font(Font.SANS_SERIF, 3, 9));
 
@@ -72,17 +50,21 @@ public class PrintingApi {
         attributes.add(new MediaPrintableArea(0,0,50, 97, Size2DSyntax.MM));
 
         textPane.setMargin(new Insets(20, 20, 20, 20));
-        //textPane.setText("test text string - Hello World! \n Are you there?");
-        textPane.setText(label);
 
-
-        textPane.print(null, null, true, PrintServiceLookup.lookupDefaultPrintService(), attributes, true);
-
-    }
-    @RequestMapping(path = "/health", method = GET)
-    public ResponseEntity<String> health() {
-        return ResponseEntity.ok("ok");
+        try {
+            textPane.print(null, null, true, PrintServiceLookup.lookupDefaultPrintService(), attributes, true);
+            return ResponseEntity.ok("Printed");
+        } catch (PrinterException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
     }
 
+    private String toString(List<String> lines) {
+        StringBuilder result = new StringBuilder();
+        for(String line : lines) {
+            result.append(line);
+        }
+        return result.toString();
+    }
 
 }
